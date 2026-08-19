2606db017faf145bac328279af09460ac9920f51
bsh <qotngus72@naver.com>
2026-08-19 10:16:38 +0900

    관광 장소 테이블 생성

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -14,3 +14,27 @@ CREATE TABLE users (
 
     CONSTRAINT uk_users_email UNIQUE (email)
 );
+
+
+CREATE TABLE places (
+    place_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    address VARCHAR(255) NOT NULL,
+
+    latitude DECIMAL(10, 7) NOT NULL,
+    longitude DECIMAL(10, 7) NOT NULL,
+
+    category VARCHAR(50) NOT NULL,
+    image_url VARCHAR(500),
+
+    created_by BIGINT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_places_created_by
+        FOREIGN KEY (created_by)
+            REFERENCES users(user_id)
+);
